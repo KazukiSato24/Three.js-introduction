d6794697dd3b146032c844e991b3a480a2ee6aeb
Kazuki Sato <kazukisato970817@gmail.com>
2022-06-23 14:48:47 +0900

    ADD:background

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import * as THREE from "./build/three.module.js";
-//シーン導入の確認
-const scene = new THREE.Scene();
-console.log(scene);
+
+let scene, camera, renderer;
+
+//シーンを追加
+scene = new THREE.Scene();
+
+//カメラを追加(視野角,アスペクト比,開始距離,終了距離)
+camera = new THREE.PerspectiveCamera(
+  50,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+
+//レンダラーを追加
+renderer = new THREE.WebGLRenderer({ alpha: true });
+document.body.appendChild(renderer.domElement)
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+renderer.render(scene, camera);
